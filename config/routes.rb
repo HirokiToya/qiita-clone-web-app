f3b10e2d52d64032403ff714c9ac3e885c12731f
Toya Hiroki <t.hiro.0425@icloud.com>
2018-03-02 01:14:10 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  get 'users/index'
+
+  get "/"=>"home#top"
+  get "/signup"=>"users#new"
+  get "/login"=>"users#login"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
