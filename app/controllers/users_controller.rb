29d395cdada1fa5ca75bfc2645c1daf8b7830186
Toya Hiroki <t.hiro.0425@icloud.com>
2018-03-02 12:40:57 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,49 @@ class UsersController < ApplicationController
   def index
   end
 
+  def show
+  	@user = User.find(params[:id])
+  end
+
+
   def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:notice] = "ユーザー登録が完了しました"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
   end
   
+  def edit
+  	@user = User.find_by(params[:id])
+  end
+
+  def update
+  	@user = User.find_by(params[:id])
+  	@user.name = params[:name]
+  	@user.email = params[:email]
+  	@user.password = params[:password]
+
+  	if @user.save
+  		flash[:notice] = "ユーザー情報を編集しました"
+  		redirect_to @user
+  	else
+  		render("users/edit")
+  	end
+  end
 
 
 end
+
+
+private
+
+  def user_params
+  	params.require(:user).permit(:name, :email, :password)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
   get 'users/index'
-
   get "/"=>"home#top"
   get "/signup"=>"users#new"
   get "/login"=>"users#login"
+  post "users/:id/update" => "users#update"
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
